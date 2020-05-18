e83420d077b0a8eb7b1ba6353e0919c175e690cd
WonTaeGO <gowontae@gmail.com>
2020-05-18 16:08:22 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ import { withStyles } from '@material-ui/core/styles';
 const styles = theme => ({
   root: {
     width: '100%',
-    marginTop: theme.spacing.unit * 3,
+    marginTop: theme.spacing(3),
     overflowX:"auto"
   },
   table:{
@@ -21,34 +21,22 @@ const styles = theme => ({
   }
 })
 
-const customers = [{
-  'id': 1,
-  'image': 'https://placeimg.com/64/64/any',
-  'name': '고원태',
-  'birthday': '971222',
-  'gender': '남자',
-  'job': '대학생'
-},
-{
-  'id': 2,
-  'image': 'https://placeimg.com/64/64/any',
-  'name': '홍길동',
-  'birthday': '960122',
-  'gender': '남자',
-  'job': '대학생'
-},
-{
-  'id': 3,
-  'image': 'https://placeimg.com/64/64/any',
-  'name': '이순신',
-  'birthday': '961212',
-  'gender': '남자',
-  'job': '대학생'
-}
-
-]
-
 class App extends Component {
+
+  state = {
+    customers:""
+  }
+
+  componentDidMount() {
+    this.callApi().then(res => this.setState({customers: res})).catch(err => console.log(err));
+  }
+
+  callApi = async() => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
     const { classes } = this.props;
     return (
@@ -65,8 +53,7 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-          {
-            customers.map(c => {
+            { this.state.customers ? this.state.customers.map(c => {
               return (
                 <Customer
                   key={c.id}
@@ -78,7 +65,7 @@ class App extends Component {
                   job={c.job}
                 />
                 );
-              })
+              }) : "" 
             }
           </TableBody>
         </Table>
--- server.js
+++ server.js
@@ -6,8 +6,34 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'})
-});
+app.get('/api/customers', (req,res) => {
+    res.send([
+          {
+            'id': 1,
+            'image': 'https://placeimg.com/64/64/any',
+            'name': '고원태',
+            'birthday': '971222',
+            'gender': '남자',
+            'job': '대학생'
+          },
+          {
+            'id': 2,
+            'image': 'https://placeimg.com/63/63/any',
+            'name': '홍길동',
+            'birthday': '960122',
+            'gender': '남자',
+            'job': '대학생'
+          },
+          {
+            'id': 3,
+            'image': 'https://placeimg.com/62/62/any',
+            'name': '이순신',
+            'birthday': '961212',
+            'gender': '남자',
+            'job': '대학생'
+          }
+          
+    ]);
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
