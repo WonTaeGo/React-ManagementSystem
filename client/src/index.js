7a858a1014ac592d772998c355e91743c76c22ad
WonTaeGO <gowontae@gmail.com>
2020-05-20 11:31:17 +0900

    Update UI/UX & Add Customer Search Module

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles'
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily:'"Noto Sans KR", serif',
+  }
+})
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <MuiThemeProvider theme={theme}><App /></MuiThemeProvider>,
   document.getElementById('root')
 );
 
@@ -15,3 +20,4 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+;
